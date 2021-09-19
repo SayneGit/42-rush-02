9c1ba7af14d51c4c9a6048471defc1f61d83b716
Ben <souleaub@gmail.com>
2021-09-19 12:03:43 +0200

    Update ft_find.c

--- ex00/srcs/files/ft_find.c
+++ ex00/srcs/files/ft_find.c
@@ -6,7 +6,7 @@
 /*   By: bsouleau <bsouleau@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 10:55:16 by rgarcia           #+#    #+#             */
-/*   Updated: 2021/09/19 11:54:01 by bsouleau         ###   ########lyon.fr   */
+/*   Updated: 2021/09/19 12:03:37 by bsouleau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 void	ft_find_zero(char **file_contents, int nbrzero)
 {
-	char	str[10];
+	char	str[11];
 	int		i;
 
-	i = 0;
+	i = 1;
+	str[0] = '1';
 	while (i < nbrzero)
 	{
 		str[i] = '0';
@@ -38,11 +39,12 @@ void	ft_find_digit(char **file_contents, char *number_string, int index)
 
 void	ft_find_hundred(char **file_contents)
 {
-	char	str[3];
+	char	str[4];
 
-	str[0] = '0';
+	str[0] = '1';
 	str[1] = '0';
-	str[2] = '\0';
+	str[2] = '0';
+	str[3] = '\0';
 	ft_putstr(ft_search_letters(file_contents, str));
 }
 
