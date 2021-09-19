a28ae03b3d5b3003673d6fb3decbfad0dd2af4bb
romugarc <romualdgarcia4@gmail.com>
2021-09-19 11:08:16 +0200

    Ajout de find_digit dans srcs, la lib et le makefile, nettoyage des erreurs de compilation de search_letters

--- ex00/srcs/utils/ft_find_digit.c
+++ ex00/srcs/utils/ft_find_digit.c
@@ -1,39 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_search_letters.c                                :+:      :+:    :+:   */
+/*   ft_find_digit.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgarcia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/19 08:31:43 by rgarcia           #+#    #+#             */
-/*   Updated: 2021/09/19 11:05:04 by rgarcia          ###   ########lyon.fr   */
+/*   Created: 2021/09/19 10:55:16 by rgarcia           #+#    #+#             */
+/*   Updated: 2021/09/19 10:58:23 by rgarcia          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_utils.h"
 
-char	*ft_search_letters(char **file, char *to_find)
+void	ft_find_digit(char **file, char *number_string, int index)
 {
-	int		i;
-	int		len;
-	int		tofind_len;
+	char	str[2];
 
-	i = 0;
-	len = 0;
-	tofind_len = 0;
-	while (to_find[tofind_len])
-		tofind_len++;
-	while (file[len])
-		len++;
-	if (to_find[i])
-	{
-		while (i < len - 1)
-		{
-			if (ft_strcmp(file[i], to_find) == 0)
-				return (ft_get_letters(file[i + 1]));
-			i++;
-		}
-		return (0);
-	}
-	return (0);
+	str[0] = number_string[index];
+	str[1] = '\0';
+	ft_putstr(ft_search_letters(file, str));
 }
